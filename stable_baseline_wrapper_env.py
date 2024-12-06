aad0eca35a1fa519cae07aeae73a9d762ea7a5f0
Dhruv Chauhan <chauhan.dhr@northeastern.edu>
2024-12-05 21:55:50 -0500

    script to train stable baselines

--- stable_baseline_wrapper_env.py
+++ stable_baseline_wrapper_env.py
@@ -2,20 +2,21 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from gymnasium.envs.box2d import CarRacing
 
-# Create the environment
-env = CarRacing()
+def train_wrapper(time_steps=1000000):
+    # Create the environment
+    env = CarRacing()
 
-# Wrap the environment for stable baselines
-env = DummyVecEnv([lambda: env])
+    # Wrap the environment for stable baselines
+    env = DummyVecEnv([lambda: env])
 
-# Stack 4 frames
-env = VecFrameStack(env, n_stack=4)
+    # Stack 4 frames
+    env = VecFrameStack(env, n_stack=4)
 
-# Use the PPO algorithm with the CnnPolicy
-model = PPO("CnnPolicy", env, verbose=1)
+    # Use the PPO algorithm with the CnnPolicy
+    model = PPO("CnnPolicy", env, verbose=1)
 
-# Train the agent
-model.learn(total_timesteps=10000)
+    # Train the agent
+    model.learn(total_timesteps=time_steps)
 
-# Save the model
-model.save("ppo_car_racing")
+    # Save the model
+    model.save("ppo_car_racing_wrapper_env")
--- train_stable_baselines.py
+++ train_stable_baselines.py
@@ -3,36 +3,32 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from self_train import WrapperEnv
 
-
-# env = DiscretizedCarRacing()
-env = gym.make('CarRacing-v3', render_mode='rgb_array')
-
-
-# Wrap the environment with DummyVecEnv (Stable-Baselines3 requires vectorized environments)
-env = DummyVecEnv([lambda: env])
-print(env.observation_space)
-
-# env = WrapperEnv()
-# env = DummyVecEnv([lambda: env])
-# print(env.observation_space)
-
-# Define PPO model
-model = PPO('CnnPolicy', env, verbose=1)
-# model = DQN('CnnPolicy', env, verbose=1)
+def train_regular(timesteps = 1000000):
+    # env = DiscretizedCarRacing()
+    env = gym.make('CarRacing-v3', render_mode='rgb_array')
 
 
-# Train the model
-model.learn(total_timesteps=1000000)  # You can increase the timesteps for better training
+    # Wrap the environment with DummyVecEnv (Stable-Baselines3 requires vectorized environments)
+    env = DummyVecEnv([lambda: env])
+    print(env.observation_space)
 
-# Save the model
-model.save("ppo_carracing_more")
+    # Define PPO model
+    model = PPO('CnnPolicy', env, verbose=1)
+    # model = DQN('CnnPolicy', env, verbose=1)
 
-# Evaluate the model
-obs = env.reset()
-for _ in range(1000):  # Test for 1000 steps (you can adjust based on your needs)
-    action, _states = model.predict(obs)
-    obs, rewards, dones, infos = env.step(action)
-    env.render()
 
-# Close the environment after testing
-env.close()
+    # Train the model
+    model.learn(total_timesteps=timesteps)  # You can increase the timesteps for better training
+
+    # Save the model
+    model.save("ppo_carracing_regular_env")
+
+# # Evaluate the model
+# obs = env.reset()
+# for _ in range(1000):  # Test for 1000 steps (you can adjust based on your needs)
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, infos = env.step(action)
+#     env.render()
+
+# # Close the environment after testing
+# env.close()
